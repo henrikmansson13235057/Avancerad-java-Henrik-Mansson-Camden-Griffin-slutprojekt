9cffce7e2c6a9fcd7a40d9294f0b2f855af84294
Lueas <57628472+Lueas@users.noreply.github.com>
2024-12-26 14:00:22 +0100

    Delete Frontend directory

--- Frontend/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.frontend {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires com.dlsc.formsfx;
-    requires com.google.gson;
-    opens com.example.frontend to com.google.gson, javafx.fxml;
-    exports com.example.frontend;
-}
